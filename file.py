f7f8f6ff425dc180b04c56c31fb808e61671c448
sunnynewbie <sunny.ptel5497@gmail.com>
2022-12-27 10:24:39 +0530

    init

--- file.py
+++ file.py
@@ -1,28 +1,27 @@
-import numpy as np
 import os
-from tflite_model_maker.config import QuantizationConfig
-from tflite_model_maker.config import ExportFormat
-from tflite_model_maker import model_spec
-from tflite_model_maker import object_detector
 
+import numpy as np
 import tensorflow as tf
+from tflite_model_maker import image_classifier, model_spec, object_detector
+from tflite_model_maker.config import ExportFormat, QuantizationConfig
+from tflite_model_maker.image_classifier import DataLoader
+
 assert tf.__version__.startswith('2')
 
 tf.get_logger().setLevel('ERROR')
 from absl import logging
+
 logging.set_verbosity(logging.ERROR)
 
-image_path = tf.keras.utils.get_file(
-      'flower_photos.tgz',
-      'https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz',
-      extract=True)
-image_path = os.path.join(os.path.dirname(image_path), 'flower_photos')
 
-from tflite_model_maker import image_classifier
-from tflite_model_maker.image_classifier import DataLoader
+folder_path = "C:\\Users\\sunny\\downloads\\OneDrive_2022-12-01\\Models With Labels_ALL IN ONE FOLDER"
 
-data = DataLoader.from_folder(image_path)
-train_data, test_data = data.split(0.9)
-model = image_classifier.create(train_data)
+
+data = DataLoader.from_folder(folder_path)
+print(data)
+train_data, test_data = data.split(0.75)
+model = image_classifier.create(train_data,epochs=5)
 loss, accuracy = model.evaluate(test_data)
+print(loss);
+print(accuracy);
 model.export(export_dir='.')
